44074b5c3d84431ab5e5e9a4bd5dbce9d53185e5
Ola Czyrnek <aleksandra@czyrnek.net>
2025-06-01 19:32:11 +0200

    Update server.ts

--- backend/cms/src/server.ts
+++ backend/cms/src/server.ts
@@ -1,15 +1,24 @@
+import express from 'express'
 import payload from 'payload'
 import dotenv from 'dotenv'
 import config from './payload.config.js'
 
 dotenv.config()
 
+const app = express()
+
 const start = async () => {
   await payload.init({
     config,
-  })
+    express: app,
+  } as any)
 
-  payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 3000
+
+  app.listen(port, () => {
+    payload.logger.info(`Server is listening on port ${port}`)
+    payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+  })
 }
 
 start()

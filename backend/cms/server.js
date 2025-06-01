016774d47099bdbd35611b8c737d182185cb5662
Ola Czyrnek <aleksandra@czyrnek.net>
2025-06-01 15:09:08 +0200

    Change server location

---
+++ backend/cms/server.js
@@ -0,0 +1,16 @@
+import config from './src/payload.config.js';
+
+import payload from 'payload';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const start = async () => {
+  await payload.init({
+    config,
+  });
+
+  payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
+};
+
+start();

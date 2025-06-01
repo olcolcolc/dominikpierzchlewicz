8f2c304484333435a8902a67b106e6347159a579
Ola Czyrnek <aleksandra@czyrnek.net>
2025-06-01 19:20:00 +0200

    Revert "Install 4.0.3 payload & tsc-alias"
    This reverts commit 0f9d0836bca6371584e29224d3158d1863f9e323.

--- backend/cms/src/server.ts
+++ backend/cms/src/server.ts
@@ -1,23 +1,15 @@
 import payload from 'payload'
 import dotenv from 'dotenv'
-import express from 'express'
 import config from './payload.config.js'
 
 dotenv.config()
 
-const app = express()
-const PORT = process.env.PORT || 3000
-
 const start = async () => {
   await payload.init({
-    express: app, // <- podajemy Express app!
     config,
   })
 
-  app.listen(PORT, () => {
-    payload.logger.info(`Server is listening on port ${PORT}`)
-    payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
-  })
+  payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
 }
 
 start()

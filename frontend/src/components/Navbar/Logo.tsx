ce3b349baf83fc69fb4650afe20f81787ecd66d0
Ola Czyrnek <aleksandra@czyrnek.net>
2025-07-21 08:52:57 +0200

    Adjust media queries

--- frontend/src/components/Navbar/Logo.tsx
+++ frontend/src/components/Navbar/Logo.tsx
@@ -1,8 +1,9 @@
 import styled from "@emotion/styled";
-import { Link } from "react-router-dom";
+import { Link, type LinkProps } from "react-router-dom";
 import type { ReactNode } from "react";
+import React from "react";
 
-const LogoWrapper = styled(Link)(({ theme }) => ({
+const LogoLink = styled(Link)(({ theme }) => ({
   fontSize: "4rem",
   height: "100%",
   fontWeight: "bold",
@@ -12,30 +13,36 @@ const LogoWrapper = styled(Link)(({ theme }) => ({
   textDecoration: "none",
   ...theme.animations.hoverLift,
   mixBlendMode: "difference",
+  display: "inline-block",
   [theme.media.tablet]: {
     fontSize: "3rem",
   },
   "@media (max-width: 930px)": {
-    width: "500px",
-  },
-  "@media (max-width: 700px)": {
     fontSize: "2.5rem",
-    width: "400px",
+    width: "260px",
   },
   "@media (max-width: 570px)": {
     fontSize: "2rem",
-    width: "300px",
+    width: "210px",
   },
-  "@media (max-width: 370px)": {
-    fontSize: "1.6rem",
-    width: "100px",
+  "@media (max-width: 470px)": {
+    fontSize: "1.2rem",
+    width: "150px",
   },
 }));
 
 type LogoProps = {
   children: ReactNode;
-};
+} & LinkProps;
 
-export const Logo = ({ children }: LogoProps) => {
-  return <LogoWrapper to="/">{children}</LogoWrapper>;
-};
+export const Logo = React.forwardRef<HTMLAnchorElement, LogoProps>(
+  ({ children, ...props }, ref) => {
+    return (
+      <LogoLink {...props} ref={ref}>
+        {children}
+      </LogoLink>
+    );
+  }
+);
+
+Logo.displayName = "Logo";
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,8 @@
+// src/components/Navbar/Navbar.tsx
 import styled from "@emotion/styled";
 import { Logo } from "./Logo";
 import { HamburgerMenu } from "./HamburgerMenu";
+import { useEffect, useRef } from "react";
 
 const Wrapper = styled.nav(({ theme }) => ({
   position: "fixed",
@@ -20,10 +22,32 @@ const Wrapper = styled.nav(({ theme }) => ({
   },
 }));
 
-export const Navbar = () => {
+type NavbarProps = {
+  onLogoWidthMeasured: (width: number) => void;
+};
+
+export const Navbar = ({ onLogoWidthMeasured }: NavbarProps) => {
+  const logoRef = useRef<HTMLAnchorElement>(null);
+
+  useEffect(() => {
+    const updateLogoWidth = () => {
+      if (logoRef.current) {
+        onLogoWidthMeasured(logoRef.current.offsetWidth);
+      }
+    };
+
+    updateLogoWidth();
+
+    window.addEventListener("resize", updateLogoWidth);
+
+    return () => window.removeEventListener("resize", updateLogoWidth);
+  }, [onLogoWidthMeasured]);
+
   return (
     <Wrapper>
-      <Logo>Dominik Pierzchlewicz</Logo>
+      <Logo to="/" ref={logoRef}>
+        Dominik Pierzchlewicz
+      </Logo>
       <HamburgerMenu />
     </Wrapper>
   );

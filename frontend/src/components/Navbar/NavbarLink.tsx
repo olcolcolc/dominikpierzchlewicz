53f6a66abc0dedc24051fb4a6f5b8c3724e02712
Ola Czyrnek <aleksandra@czyrnek.net>
2025-09-07 18:05:07 +0200

    Reapply "Navbar changes"
    This reverts commit ba69c23935d21b83cd327fdf6e03c066b2b50bca.

--- frontend/src/components/Navbar/NavbarLink.tsx
+++ frontend/src/components/Navbar/NavbarLink.tsx
@@ -4,26 +4,38 @@ import styled from "@emotion/styled";
 const StyledLink = styled(Link)(({ theme }) => ({
   fontSize: "5rem",
   textDecoration: "none",
-  textTransform: "capitalize",
-  color: theme.colors.text,
+  color: "black",
+  letterSpacing: "-0.05em",
+  fontFamily: `${theme.fonts.dmSans}`,
   cursor: "pointer",
   transition: "color 0.3s ease",
   "&:hover": {
     color: theme.colors.hover,
   },
-
   [theme.media.mobileL]: {
     fontSize: "5rem",
   },
 }));
 
+const NumberSpan = styled.span({
+  fontWeight: "bold",
+});
+
 type NavbarAltLinkProps = {
   to: string;
   children: React.ReactNode;
   onClick?: () => void;
+  index: number;
 };
 
-export const NavbarLink = ({ to, children, onClick }: NavbarAltLinkProps) => {
+export const NavbarLink = ({
+  to,
+  children,
+  onClick,
+  index,
+}: NavbarAltLinkProps) => {
+  const formattedIndex = String(index).padStart(2, "0");
+
   return (
     <StyledLink
       to={to}
@@ -33,6 +45,7 @@ export const NavbarLink = ({ to, children, onClick }: NavbarAltLinkProps) => {
       duration={500}
       onClick={onClick}
     >
+      <NumberSpan>{formattedIndex} / </NumberSpan>
       {children}
     </StyledLink>
   );

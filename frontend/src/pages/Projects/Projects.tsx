cdc30e9ec61e5b3a342bafadf196c07f36c5d7cb
Ola Czyrnek <aleksandra@czyrnek.net>
2025-11-29 21:27:54 +0100

    feat: Revamp

--- frontend/src/pages/Projects/Projects.tsx
+++ frontend/src/pages/Projects/Projects.tsx
@@ -1,40 +1,94 @@
-import { ListDisplay } from "../../components/ListDisplay/ListDisplay";
+import styled from "@emotion/styled";
 import { projectsData } from "../../data/projectsData";
 import { theme } from "../../styles/theme";
-import styled from "@emotion/styled";
 
 const Wrapper = styled.div({
   width: "100%",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-start",
+  display: "block",
   [theme.media.tablet]: {
     padding: "0",
   },
 });
 
-const ListWrapper = styled.div({
+const Grid = styled.div({
   width: "100%",
-  [theme.media.tablet]: {
-    marginTop: "13rem",
-  },
-  [theme.media.mobileL]: {
-    marginTop: "11rem",
-  },
+  display: "grid",
+  gridTemplateColumns: "1fr 1fr",
+  gap: "2rem",
+
   [theme.media.mobileM]: {
-    marginTop: "10rem",
-  },
-  [theme.media.mobileS]: {
-    marginTop: "12rem",
+    gridTemplateColumns: "1fr",
+    gap: "1.2rem",
   },
 });
 
+const Card = styled.div({
+  position: "relative",
+  width: "100%",
+  aspectRatio: "1 / 1",
+  overflow: "hidden",
+  cursor: "pointer",
+
+  "&:hover img": {
+    transition: "transform 0.6s ease",
+    transform: "translateY(-3rem) ",
+  },
+
+  "&:hover .overlay": {
+    transform: "translateY(0)",
+  },
+});
+
+const Image = styled.img({
+  width: "100%",
+  height: "100%",
+  objectFit: "cover",
+  display: "block",
+  transition: "transform 0.6s ease",
+});
+
+const Overlay = styled.div({
+  position: "absolute",
+  left: 0,
+  right: 0,
+  bottom: 0,
+  padding: "0.8rem 1rem",
+  backgroundColor: "white",
+  transform: "translateY(100%)",
+  transition: "transform 0.6s ease",
+  display: "flex",
+  flexDirection: "column",
+  textTransform: "uppercase",
+  letterSpacing: "0.08em",
+  fontFamily: theme.fonts.inter,
+  fontSize: "0.8rem",
+});
+
+const Title = styled.span({
+  fontWeight: 600,
+});
+
 const Projects = () => {
+  console.log("projectsData:", projectsData);
+
   return (
     <Wrapper>
-      <ListWrapper>
-        <ListDisplay data={projectsData} />
-      </ListWrapper>
+      <Grid>
+        {projectsData.map((project) => {
+          const cover = project.gallery[0];
+
+          if (!cover) return null;
+
+          return (
+            <Card key={project.title}>
+              <Image src={cover} alt={project.title} />
+              <Overlay className="overlay">
+                <Title>{project.title}</Title>
+              </Overlay>
+            </Card>
+          );
+        })}
+      </Grid>
     </Wrapper>
   );
 };
